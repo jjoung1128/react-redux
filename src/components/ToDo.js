c0dbf6a00e78958d4fb9890e3dc6671a4741c08a
jjoung1128 <jinwoong.joung@gmail.com>
2022-01-02 12:46:47 -0600

    add redux toolkit

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,20 +1,19 @@
 import { connect } from "react-redux";
-import { actionCreators } from "../store";
+import { remove } from "../store";
 import { Link } from "react-router-dom";
 
 function ToDo({ id, text, deleteToDo }) {
     return (
         <li>
-            <Link to={`/${id}`}>
-                {text} <button onClick={deleteToDo}>DEL</button>
-            </Link>
+            <Link to={`/${id}`}>{text}</Link>
+            <button onClick={deleteToDo}>DEL</button>
         </li>
     )
 }
 
 function mapDispatchToProps(dispatch, ownProps) {
     return {
-        deleteToDo: () => dispatch(actionCreators.deleteTodo(ownProps.id))
+        deleteToDo: () => dispatch(remove(ownProps.id))
     };
 }
 
--- src/store.js
+++ src/store.js
@@ -1,38 +1,16 @@
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addToDo = (text) => {
-    return {
-        type: ADD,
-        text
-    };
-}
-
-const deleteTodo = (id) => {
-    return {
-        type: DELETE,
-        id
-    };
-}
-
-const reducer = (state = [], action) => {
-    switch (action.type) {
-        case ADD:
-            return [{ text: action.text, id: Date.now() }, ...state];
-        case DELETE:
-            return state.filter(toDo => toDo.id !== action.id);
-        default:
-            return state;
+const toDos = createSlice({
+    name: 'toDosReducer',
+    initialState: [],
+    reducers: {
+        add: (state, action) => {
+            state.push({ text: action.payload, id: Date.now() });
+        },
+        remove: (state, action) => state.filter(toDo => toDo.id !== action.payload)
     }
-}
+})
 
-const store = createStore(reducer);
+export const {add, remove} = toDos.actions;
 
-export const actionCreators = {
-    addToDo,
-    deleteTodo
-}
-
-export default store;
+export default configureStore({ reducer: toDos.reducer });
